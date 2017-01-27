f42e1aa052c23aaa0ce63bfa7bd1d46d5794ca9e
Marufenko <price.of.the.flight@hotmail.com>
2017-01-27 02:01:37 +0100

    update file names

---
+++ requests.py
@@ -0,0 +1,8 @@
+import requests
+
+r = requests.get('http://yasdelie.ru/img/prof.png')
+print(r.status_code)
+# print(r.encoding)
+# print(r.headers)
+# print(r.content)
+# print(r.text)
--- tmp.py
+++
@@ -1,3 +0,0 @@
-import requests
-r = requests.get('http://yasdelie.ru/img/profile.png')
-print(r.status_code)
